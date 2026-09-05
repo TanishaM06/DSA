9239000ca13a5c87a0aa40714f2ee828492f9d00
TanishaM06 <manetanisha06@gmail.com>
2026-09-05 17:57:32 +0530

    array sort code4

---
+++ src/array.java
@@ -0,0 +1,13 @@
+public class array {
+    public static void main (String[] args) {
+        int [] arr = {1,4,6,7,8};
+        System.out.println(sort(arr ,0 ));
+    }
+    static boolean sort(int [] arr , int i) {
+        if( i == arr.length -1){
+            return true;
+        }
+
+        return arr[i] < arr [i+1] && sort(arr , i+1);
+    }
+}
